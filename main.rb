708368dbc9fd864cb43f8314ce563b6bd333cfb7
Nik Hixson <nikilobe@gmail.com>
2025-03-10 12:19:01 -0700

    Refactor candidate filtering methods and enhance candidate data structure

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
+require 'active_support/all'
 require 'pp'
-require './candidates'
-require './filters'
+require_relative 'candidates'
+require_relative 'filters'
 
 ## Your test code can go here
 
